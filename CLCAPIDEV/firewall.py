bc4d9d003c4086f12795ec345f21c1816f145dd9
Eric Lucas <eric.lucas@centurylink.com>
2016-11-27 12:17:29 -0800

    Read Update

--- CLCAPIDEV/firewall.py
+++ CLCAPIDEV/firewall.py
@@ -5,7 +5,7 @@ import requests
 
 
 headers = {'content-type': 'application/json'}
-payload = {'username': 'eric.lucas.xxx', 'password': 'password'}
+payload = {'username': 'eric.xxx', 'password': ‘xxxxx’}
 
 r = requests.post("https://api.ctl.io/v2/authentication/login", data=payload)
 print r
